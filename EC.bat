b22cd6e6f4b2ea51a74240f4344a696ed43ab4ff
hypercrites <registry@hypercrites.de>
2017-11-06 00:08:09 +0100

    Add files via upload

---
+++ EC.bat
@@ -0,0 +1,19 @@
+@echo off
+color c
+@echo on
+@echo #######################################
+@echo ##                                   ##
+@echo ##   ##   ##  #######  #####    ##   ##
+@echo ##   ##  ##   ##   ##  ##   ##  ##   ##
+@echo ##   ## ##    ##   ##  ##   ##  ##   ##
+@echo ##   ####     ##   ##  ##   ##  ##   ##
+@echo ##   ## ##    ##   ##  ##   ##  ##   ##
+@echo ##   ##  ##   ##   ##  ##   ##  ##   ##
+@echo ##   ##   ##  #######  #####    ##   ##
+@echo ##   Launcher                        ##
+@echo ##       for Creative Media Center   ##
+@echo ##                  by hYpercrites   ##
+@echo #######################################
+@echo off
+start /d "C:\Program Files (x86)\Kodi" Kodi.exe
+@echo on
